a6817c63e239d9bfc6303eee7d01cd17bff768c2
Joao_Vitor <2013joaovitor10@Gmail.com>
2023-03-24 14:26:20 -0300

    Começando o projeto

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ function getRandomInt(min: number, max: number) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
+  
 }
 
 export default getRandomInt;
